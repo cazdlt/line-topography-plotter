90dfe3d3d23da50b6e8a74bb061aefd51707ead2
cazdlt <cazdlt@gmail.com>
2020-05-09 19:22:01 -0500

    Implementado para Colombia

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# line-topography-plotter
+# line-topography-plotter-colombia
+Mapa topográfico por líneas (implementación para la Colombia continental) <br>
+Implementación basada en [line-topography-plotter](https://github.com/sjaquemate/line-topography-plotter/)<br>
+Datos tomados de [CGIAR](http://srtm.csi.cgiar.org/) para los datos satelitales de relieve y [SERiES](https://sites.google.com/site/seriescol/shapes) para la forma del país. <br><br>
 
-Example: (color inverted)
+Ejemplo (con fondo negro): 
 
-![alt text](https://github.com/sjaquemate/line-topography-plotter/blob/master/map_gray_small.png)
+![alt text](https://github.com/cazdlt/line-topography-plotter/blob/master/plot3.png)
 
--- main.py
+++ main.py
@@ -6,29 +6,36 @@ import line_plotting
 
 if __name__ == "__main__":
 
+    
+    path = './col.tif'
+    save = True
+    plot_raster=False
+    n_lines = 50
+    out_path="plot3.png"
+    z_fraction=0.35 #height
+    n_points = 1200 # number of data points
+    colormap="winter"
+
     #import tiff map to numpy array
-    dir = 'C:/__whatever_your_directory_is__'
-    map = tiff.imread(dir + 'worldmap_(576, 1728).tiff')
+    map = tiff.imread(path)
     map = np.array(map, dtype=float)
     print('map.shape=', map.shape)
 
-    if False: #plot map
+    if plot_raster: #plot map
         plt.imshow(map)
         plt.show(block=True)
 
     map = map[::-1, :] # reverse y axis
 
     # create grid
-    n_lines = 200
-    n_points = 1700 # number of data points
-    n_int_points = 1700
+    n_int_points = n_points
     x = np.linspace(0, map.shape[1]-1, n_points, dtype=int)
     y_values = np.linspace(0, map.shape[0]-1, n_lines, dtype=int)
 
     highest_value = np.max(map)
     print('highest_value=', highest_value)
     # create figure
-    cmap = plt.get_cmap('binary')
+    cmap = plt.get_cmap(colormap)
     fig, ax = plt.subplots()
     fig.set_facecolor((1.0, 1.0, 1.0))
     ax.set_aspect('equal')
@@ -47,13 +54,13 @@ if __name__ == "__main__":
         z = map[line, x]
 
         # set all values smaller than -30000 to 0
-        remove_values = z < -30000
+        remove_values = z < -100
         z[remove_values] = 0
 
         smoothen = True
         # interpolate to n_int_points
         if smoothen:
-            tck = interpolate.splrep(x, z, s=30000000, k=1)
+            tck = interpolate.splrep(x, z, s=30000000, k=3)
             x_int = np.linspace(min(x), max(x), n_int_points)
             z_int = interpolate.splev(x_int, tck)
             y_int = np.array(np.ones(n_int_points)*y_value, dtype=int)
@@ -65,15 +72,16 @@ if __name__ == "__main__":
             z_int[remove_values] = np.nan
 
         # set dynamics colors and widths
-        colors = [cmap(0.15+0.85*value/highest_value) if not np.isnan(value) else cmap(0) for value in z_int]
-        widths = [0.1 + 0.2*value / highest_value for value in z_int]
+        # colors = [cmap(0.45+0.55*value/highest_value) if not np.isnan(value) else cmap(0) for value in x_int]
+        colors = [cmap(value/max(x)) if not np.isnan(value) else cmap(0) for value in x_int]
+        # widths = [0.1 + 0.2*value / highest_value for value in z_int]
+        widths=[0.5 for value in z_int]
 
-        line_plotting.plot_line_2d(ax, x_int, y_int, z_int, z_fraction=0.003, linewidths=widths, linecolors=colors)
+        line_plotting.plot_line_2d(ax, x_int, y_int, z_int, z_fraction=z_fraction, linewidths=widths, linecolors=colors)
 
-    save = True
     if save:
-        print('saving svg')
-        plt.savefig("graph.svg")
+        # print('saving svg')
+        # plt.savefig("graph.svg")
         print('saving png')
-        plt.savefig('plot.png', dpi=5000)
-    plt.show()
+        plt.savefig(out_path, dpi=1000)
+    # plt.show()
